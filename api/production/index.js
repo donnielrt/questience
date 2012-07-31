ce892c0faf9b379a6db6f186490fd42aa3f68070
Donniel Thomas <donniel@gmail.com>
2012-07-30 22:52:28 -0700

    Whoops, this isn't working. Trying again from scratch

--- api/production/index.js
+++
@@ -1,6 +0,0 @@
-exports.home = require('../production/home');
-exports.about = require('../production/about');
-exports.contact = require('../production/contact');
-exports.github = require('../production/github');
-exports.login = require('../production/login');
-exports.account = require('../production/account');
